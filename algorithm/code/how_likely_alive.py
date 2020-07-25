dfa33656993aa8ddf9a80860531c3eb9c17b2c95
ArkiZh <1797466468@qq.com>
2020-07-25 12:11:06 +0800

    Doing.

--- algorithm/code/how_likely_alive.py
+++ algorithm/code/how_likely_alive.py
@@ -6,10 +6,41 @@
 输入矩阵大小3*3，起点（0,0），n=2，输出0.375
 """
 
-from collections import deque
+cached_probs = {}
 
 
 def how_likely_alive(size, startx, starty, n):
+    """
+    Make sure the arguments match the following conditions.
+    size: tuple of two positive int
+    startx: non-negative int
+    starty: non-negative int
+    n: non-negative int
+    """
+    cached_val = cached_probs.get((startx, starty, n))
+    if cached_val is not None:
+        return cached_val
+    x, y = size
+    if startx < 0 or startx >= x or starty < 0 or starty >= y:
+        return 0
+    if n == 0:
+        return 1
+    prob = sum([how_likely_alive(size, startx + step[0], starty + step[1], n - 1)
+                for step in [(-1, 0), (1, 0), (0, -1), (0, 1)]]) / 4
+    cached_probs[(startx, starty, n)] = prob
+    return prob
+
+
+print(how_likely_alive((2, 2), 0, 0, 1))
+print(how_likely_alive((3, 3), 1, 1, 1))
+print(how_likely_alive((3, 3), 0, 0, 2))
+print(how_likely_alive((10, 10), 3, 5, 300))
+print(how_likely_alive((1, 2), 0, 1, 100))
+
+from collections import deque
+
+
+def how_likely_alive1(size, startx, starty, n):
     """
     Make sure the arguments match the following conditions.
     size: tuple of two positive int
@@ -32,10 +63,3 @@ def how_likely_alive(size, startx, starty, n):
             for direction in directions:
                 positions.append((cur[0] + 1, cur[1] + direction[0], cur[2] + direction[1]))
     return live_prob
-
-
-print(how_likely_alive((2, 2), 0, 0, 1))
-print(how_likely_alive((3, 3), 1, 1, 1))
-print(how_likely_alive((3, 3), 0, 0, 2))
-print(how_likely_alive((10, 10), 1, 1, 10))
-print(how_likely_alive((1, 2), 0, 1, 100))
